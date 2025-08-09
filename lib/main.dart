5856b64a54a9725503178f605e749e99b5525717
nishant <jnishantand@gmail.com>
2025-08-09 07:56:09 +0530

    login/sign up firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,21 +8,52 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const SizedBox(height: 100),
-        const Text('Login Screen', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            // Navigate to the next screen
-            Navigator.pushNamed(context, '/home');
-          },
-          child: const Text('Go to Home'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 100),
+              Center(
+                child: const Text(
+                  'Login Screen',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+
+                  _formKey.currentState?.validate();
+                },
+                child: const Text('Go to Home'),
+              ),
+            ],
+          ),
         ),
-    ],),);
+      ),
+    );
   }
 }
